1c3001bfa719aa7282444725373283efc641f969
karenoggier <karenoggier@gmail.com>
2025-12-10 10:54:53 -0300

    VPC A lista (firewall completo)

--- VPC A (Argentina)/subnet-privada-backends/ReglasFirewallBackends.sh
+++ VPC A (Argentina)/subnet-privada-backends/ReglasFirewallBackends.sh
@@ -15,13 +15,19 @@ iptables -A OUTPUT -o lo -j ACCEPT
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
-# Permitir SSH desde los backends
-iptables -A INPUT -s 10.1.10.0/24 -d 0.0.0.0/0 -p tcp --sport 1024:65535 --dport 22 -m state --state NEW -j ACCEPT
+# Permitir SSH desde el router A (falta agregar luego para la VPN)
+iptables -A INPUT -s 10.1.1.72/32 -d 0.0.0.0/0 -p tcp --sport 1024:65535 --dport 22 -m state --state NEW -j ACCEPT # ip privada del router A
 
-# Permitir que los backends se conecten a la base de datos MySQL (puerto 3306)
-iptables -A INPUT -s 10.1.10.0/24 -d 0.0.0.0/0 -p tcp --sport 1024:65535 --dport 3306 -m state --state NEW -j ACCEPT
+# Permitir SSH hacia la BDD
+iptables -A OUTPUT -s 0.0.0.0/0 -d 10.1.20.0/24 -p tcp --sport 1024:65535 --dport 22 -m state --state NEW -j ACCEPT
 
-# Permitir salida a internet a través de los Backends
+# Permitir salida MySQL (puerto 3306) hacia la BDD
+iptables -A OUTPUT -s 0.0.0.0/0 -d 10.1.20.0/24 -p tcp --sport 1024:65535 --dport 3306 -m state --state NEW -j ACCEPT
+
+# Permitir que el proxy reverso (Router A) pueda consultar la web
+iptables -A INPUT -s 10.1.1.72/32 -d 0.0.0.0/0 -p tcp --sport 1024:65535 --dport 88 -m state --state NEW -j ACCEPT # ip privada del router A
+
+# Permitir salida a internet a través del Router A
 ## DNS
 iptables -A OUTPUT -s 0.0.0.0/0 -d 0.0.0.0/0 -p udp --sport 1024:65535 --dport 53 -m state --state NEW -j ACCEPT
 iptables -A OUTPUT -s 0.0.0.0/0 -d 0.0.0.0/0 -p tcp --sport 1024:65535 --dport 53 -m state --state NEW -j ACCEPT
@@ -29,6 +35,8 @@ iptables -A OUTPUT -s 0.0.0.0/0 -d 0.0.0.0/0 -p tcp --sport 1024:65535 --dport 5
 iptables -A OUTPUT -s 0.0.0.0/0 -d 0.0.0.0/0 -p tcp --sport 1024:65535 --dport 80 -m state --state NEW -j ACCEPT
 iptables -A OUTPUT -s 0.0.0.0/0 -d 0.0.0.0/0 -p tcp --sport 1024:65535 --dport 443 -m state --state NEW -j ACCEPT
 
+#Permitir ping desde cualquier equipo conectado al tunel (falta)
+iptables -A OUTPUT -s 0.0.0.0/0 -d 0.0.0.0/0 -p icmp --icmp-type 8 -m state --state NEW -j ACCEPT # por el momento
 
 #------------POLITICAS POR DEFECTO---------------
 # Bloquear todo por defecto (DROP)
