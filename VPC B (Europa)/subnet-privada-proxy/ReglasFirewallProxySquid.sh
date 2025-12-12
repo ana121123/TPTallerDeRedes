7090f6538be56feb5c9e9f95de4853b00db0806c
karenoggier <karenoggier@gmail.com>
2025-12-12 19:20:17 -0300

    firewall del squid

--- VPC B (Europa)/subnet-privada-proxy/ReglasFirewallProxySquid.sh
+++ VPC B (Europa)/subnet-privada-proxy/ReglasFirewallProxySquid.sh
@@ -1,45 +1,51 @@
 #!/bin/bash
 
 # Limpiar reglas
+iptables -P INPUT ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -P FORWARD ACCEPT
 iptables -F OUTPUT
 iptables -F INPUT
 iptables -F FORWARD
 
 #------------LOOPBACK---------------
+# Permitir tráfico por loopback
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
 
-#------------CONEXIONES ESTABLECIDAS---------------
+#------------CONEXIONES YA ESTABLECIDAS---------------
+# Permitir que entren paquetes de respuestas a conexiones que el proxy inició
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+# Permitir que salgan respuestas a conexiones que entraron
 iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
-#------------ACCESO SSH---------------
-# Permitir SSH desde Router B (Bastión local - 10.2.1.0/24)
-iptables -A INPUT -s 10.2.1.0/24 -p tcp --dport 22 -m state --state NEW -j ACCEPT
-# Opcional: Permitir SSH desde la VPN (Router A)
-iptables -A INPUT -s 10.10.10.0/24 -p tcp --dport 22 -m state --state NEW -j ACCEPT
+#------------REGLAS DE ENTRADA (INPUT)---------------
+# SSH - Permitir acceso solo desde el Router B (Bastión)
+# IP Privada Router B: 10.2.1.236
+iptables -A INPUT -s 10.2.1.236/32 -p tcp --sport 1024:65535 --dport 22 -m state --state NEW -j ACCEPT
 
-#------------SERVICIO SQUID PROXY---------------
-# Permitir que la VPC A (10.1.0.0/16) se conecte al puerto 3128
-# Esto viaja por el túnel WireGuard -> Router B -> Proxy
-iptables -A INPUT -s 10.1.0.0/16 -p tcp --dport 3128 -m state --state NEW -j ACCEPT
+# SQUID - Permitir tráfico al puerto 3128
+# Como Router B hace NAT (Masquerade), el tráfico de la VPN llega con la IP del Router B
+iptables -A INPUT -s 10.2.1.236/32 -p tcp --sport 1024:65535 --dport 3128 -m state --state NEW -j ACCEPT
 
-#------------SALIDA A INTERNET---------------
-# Permitir al Proxy salir a internet (HTTP/HTTPS/DNS) para buscar las páginas que le piden
-## DNS
-iptables -A OUTPUT -s 0.0.0.0/0 -d 0.0.0.0/0 -p udp --sport 1024:65535 --dport 53 -m state --state NEW -j ACCEPT
-iptables -A OUTPUT -s 0.0.0.0/0 -d 0.0.0.0/0 -p tcp --sport 1024:65535 --dport 53 -m state --state NEW -j ACCEPT
-## HHTP Y HHTPS
-iptables -A OUTPUT -s 0.0.0.0/0 -d 0.0.0.0/0 -p tcp --sport 1024:65535 --dport 80 -m state --state NEW -j ACCEPT
-iptables -A OUTPUT -s 0.0.0.0/0 -d 0.0.0.0/0 -p tcp --sport 1024:65535 --dport 443 -m state --state NEW -j ACCEPT
+# ICMP - Permitir ping desde la red interna (Router B y VPN)
+# Permitimos toda la subred de infraestructura o la IP del router
+iptables -A INPUT -s 10.2.0.0/16 -p icmp --icmp-type 8 -m state --state NEW -j ACCEPT
 
-#------------PING (ICMP)---------------
-# Permitir ping desde Router B y VPN
-iptables -A INPUT -s 10.2.1.0/24 -p icmp --icmp-type 8 -j ACCEPT
-iptables -A INPUT -s 10.10.10.0/24 -p icmp --icmp-type 8 -j ACCEPT
-iptables -A OUTPUT -p icmp --icmp-type 8 -j ACCEPT
+#------------REGLAS DE SALIDA (OUTPUT)---------------
+# DNS - Para resolver dominios (Google, Gobierno, etc.)
+iptables -A OUTPUT -p udp --dport 53 -m state --state NEW -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 53 -m state --state NEW -j ACCEPT
+
+# HTTP/HTTPS - Salida a internet para buscar las páginas web
+iptables -A OUTPUT -p tcp --dport 80 -m state --state NEW -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 443 -m state --state NEW -j ACCEPT
+
+# ICMP - Hacer ping hacia afuera (troubleshooting)
+iptables -A OUTPUT -p icmp --icmp-type 8 -m state --state NEW -j ACCEPT
 
 #------------POLITICAS POR DEFECTO---------------
+# Bloquear todo por defecto (DROP)
 iptables -P INPUT DROP
 iptables -P OUTPUT DROP
 iptables -P FORWARD DROP
